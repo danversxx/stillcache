00cccc88417c7e2025cadb6604c41f488010c8de
Jay Patel <j@Jays-Mac-mini.local>
2026-02-23 22:17:24 +0000

    Add next.config.js to whitelist Cloudflare R2 images

--- next.config.js
+++ next.config.js
@@ -4,14 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'framerusercontent.com',
-      },
-      {
-        protocol: 'https',
         hostname: 'pub-67d300fe11f74bb2b7b044b304971a5c.r2.dev',
+        pathname: '/**',
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
